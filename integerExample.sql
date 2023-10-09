3102d774c2b9d1e990bd5f0fe97bb63a141f96d4
Abel Scholl <116765585+Abel-Scholl@users.noreply.github.com>
2023-10-09 16:50:49 -0400

    Update integerExample.sql

--- integerExample.sql
+++ integerExample.sql
@@ -1,35 +1,3 @@
-
--- Integer types example using TINYINT
--- -- The following code will create a table with two TINYINT fields, one signed and the other unsigned.
--- -- This example demonstrates the range of values that the TINYINT data type can hold.
-
-CREATE TABLE TinyIntExample (
-	tinyIntsSigned TINYINT,
-    tinyIntsUnsigned TINYINT UNSIGNED
-);
--- Signed TINYINT Example
-INSERT INTO TinyIntExample (tinyIntsSigned) VALUES (0), (127), (-128); -- insert max values within range limits into the table
--- These next two lines will throw errors, becasue the values being inserted are out of range.
-INSERT INTO TinyIntExample (tinyIntsSigned) VALUES (128); -- Out of Range Value
-INSERT INTO TinyIntExample (tinyIntsSigned) VALUES (-129); -- Out of Range Value
-
--- show all signed tinyint data in the table
-SELECT tinyIntsSigned FROM TinyIntExample WHERE tinyIntsSigned IS NOT NULL;
-
--- Unsigned TINYINT Example
-INSERT INTO TinyIntExample (tinyIntsUnsigned) VALUES (0), (255); -- insert max values within range limits into the table
--- These next two lines will throw errors, becasue the values being inserted are out of range.
-INSERT INTO TinyIntExample (tinyIntsUnsigned) VALUES (-1); -- Out of Range Value
-INSERT INTO TinyIntExample (tinyIntsUnsigned) VALUES (256); -- Out of Range Value
-
--- show all unsigned tinyint data in the table
-SELECT tinyIntsUnsigned FROM TinyIntExample WHERE tinyIntsUnsigned IS NOT NULL;
-
- DROP TABLE TinyIntExample; -- used to delete the table if necessary for starting over
-
-
--- -------------------------------------------------------------------------------------------- --
-
 -- Comprehensive Example using all integer data types
 -- -- The following code creates a table of all integer types, both signed and unsigned
 -- -- in order to demonstrate the value ranges of each type.
@@ -47,10 +15,12 @@ CREATE TABLE IntegerExample (
 );
 
 -- insert upper bound values for each field.
-INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
+INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, 
+mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
     VALUES (127,255,32767,65535,8388607,16777215,2147483647,4294967295,9223372036854775807,18446744073709551615);
 -- insert zero, a lower bound value for unsigned types and middle bound for signed types.
-INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
+INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, 
+mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
     VALUES (0,0,0,0,0,0,0,0,0,0);
 -- insert lower bound values for signed types only (because negative values are out of range for unsigned types)
 INSERT INTO IntegerExample (tinyIntsSigned, smallIntsSigned, mediumIntsSigned, intsSigned, bigIntsSigned) 
