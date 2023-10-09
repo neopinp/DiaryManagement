a6995d0818d49e5d48d7a160e1a7c61ef1a0566f
Evan Spillane (Laptop) <evan.spillane1@marist.edu>
2023-10-09 18:22:27 -0400

    Organization

--- Phase 03/integerExample.sql
+++ Phase 03/integerExample.sql
@@ -15,10 +15,12 @@ CREATE TABLE IntegerExample (
 );
 
 -- insert upper bound values for each field.
-INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
+INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, 
+mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
     VALUES (127,255,32767,65535,8388607,16777215,2147483647,4294967295,9223372036854775807,18446744073709551615);
 -- insert zero, a lower bound value for unsigned types and middle bound for signed types.
-INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
+INSERT INTO IntegerExample (tinyIntsSigned, tinyIntsUnsigned, smallIntsSigned, smallIntsUnsigned, 
+mediumIntsSigned, mediumIntsUnsigned, intsSigned, intsUnsigned, bigIntsSigned, bigIntsUnsigned) 
     VALUES (0,0,0,0,0,0,0,0,0,0);
 -- insert lower bound values for signed types only (because negative values are out of range for unsigned types)
 INSERT INTO IntegerExample (tinyIntsSigned, smallIntsSigned, mediumIntsSigned, intsSigned, bigIntsSigned) 
